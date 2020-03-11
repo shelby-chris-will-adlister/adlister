892341dcacc42ef7ef0419f63dcd6331e7c6439e
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-03-11 09:32:33 -0500

    ....

--- src/main/java/com/codeup/adlister/controllers/ContractServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ContractServlet.java
@@ -2,7 +2,10 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.dao.MySQLContractsDao;
+import com.codeup.adlister.models.Contract;
+import com.codeup.adlister.util.Config;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "controllers.ContractServlet", urlPatterns = "/contract")
 public class ContractServlet extends HttpServlet {
+    private MySQLContractsDao contractsDao;
+    public void init(){
+        contractsDao = new MySQLContractsDao(new Config())
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getServletPath();
         try{
@@ -43,7 +51,23 @@ public class ContractServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/contracts/individual.jsp").forward(request, response);
     }
 
+    private void addNewContract(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        long id = request.getParameter("id");
+        long user_id = request.getParameter("user_id");
+        long role_id = request.getParameter("role_id");
+        String desc = request.getParameter("description");
+        String title = request.getParameter("title");
+        String country = request.getParameter("country");
+        float reward = request.getParameter("reward");
+        Contract newContract = new Contract(id, user_id,title, desc, country,reward);
+        contractsDao.insert(newContract);
+        response.sendRedirect("list");
+    }
+
     private void listContract(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException{
-        List<Contract> contractList = MySQLContractsDao.all();
+        List<Contract> contractList = contractsDao.all();
+        request.setAttribute("list", contractList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("somepage(waitingonchris).jsp");
+        dispatcher.forward(request, response);
     }
 }
