7c1cf8b056aa6e26d1ae76de899c36ee4549d646
William Stephens <william.a.stephens17@gmail.com>
2020-03-11 16:46:24 -0500

    Changes from Christ

--- src/main/java/com/codeup/adlister/controllers/IndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/IndexServlet.java
@@ -1,5 +1,4 @@
 package com.codeup.adlister.controllers;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/welcome")
 public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/index.jsp").forward(request, response);
